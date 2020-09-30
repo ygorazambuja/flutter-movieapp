f03525c11968296b8b2387731f2ef868771b4f1f
Ygor Azambuja <ygorazambuja@gmail.com>
2020-09-30 07:41:06 -0400

    refactor: remove unused imports

--- lib/app/modules/drawer_page/drawer_page_page.dart
+++ lib/app/modules/drawer_page/drawer_page_page.dart
@@ -1,7 +1,6 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:random_color/random_color.dart';
